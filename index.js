a160c71d1ea7e999ec9a40ebeb86787faf6f9c2f
maksudalam <maksud.cdz@gmail.com>
2022-10-25 22:39:29 +0600

    course data

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const app=express();
 const cors = require('cors')
 const port=process.env.PORT||5000;
 const coursesCategory=require('./data/courses.json')
+const deatiles=require('./data/deatiles.json')
+app.use(cors())
 app.get('/',(req,res)=>{
     res.send('Education API Running')
 })
+
+app.get('/coursesCategory',(req,res)=>{
+    res.send(coursesCategory)
+})
+app.get('/deatiles',(req,res)=>{
+    res.send(deatiles)
+})
+
+
 app.listen(port,()=>{
     console.log('Education Server running on port',port)
 })
